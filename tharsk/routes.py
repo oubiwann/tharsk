9725a8b22cc8ab3a61c60d8731f4b694d883762d
Duncan McGreggor <duncan@dreamhost.com>
2012-06-19 00:49:39 -0400

    Added route for assets and updated root route.

--- tharsk/routes.py
+++ tharsk/routes.py
@@ -1,6 +1,22 @@
+from twisted.python import log
+from twisted.python.filepath import FilePath
+from twisted.web.static import File
+from twisted.web.template import Element, renderer, XMLFile, XMLString
+
 from klein import route
 
 
 @route("/")
 def root(request):
-    return "Hello, world!"
+    filepath = FilePath('templates/index.xml')
+    print filepath
+    print filepath.exists()
+    class DumbTemplate(Element):
+        #loader = XMLFile(FilePath('templates/index.xml'))
+        loader = XMLString(FilePath('templates/index.xml').getContent())
+    return DumbTemplate()
+
+
+@route("/assets/")
+def assets(request):
+    return File("./assets")
