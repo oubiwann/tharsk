837ae60ce618f45ad416e63f588a90ab51672d0c
Duncan McGreggor <duncan@dreamhost.com>
2012-06-19 22:33:44 -0400

    Added meta data for use in templates, etc.

---
+++ tharsk/meta.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8
+displayName = "Φarsk"
+libraryName = "tharsk"
+version = "0.1"
+author = "Duncan McGreggor"
+startingYear = 2012
+authorEmail = "oubiwann@twistedmatrix.com"
+license = "BSD"
+url = "http://github.com/oubiwann/tharsk"
+description = "An open source, interactive Proto-Indo-European search tool"
+longDescription = description
+requires = [
+    "twisted",
+    "zope.component",
+    ]
