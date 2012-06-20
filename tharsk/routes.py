23cadd0db44dd95b8ff8f74ec7985853356f7ae8
Duncan McGreggor <duncan@dreamhost.com>
2012-06-20 01:18:24 -0400

    Added routes for the new links.

--- tharsk/routes.py
+++ tharsk/routes.py
@@ -3,12 +3,27 @@ from twisted.web.static import File
 
 from klein import route
 
-from tharsk import elements
+from tharsk import pages
 
 
 @route("/")
 def root(request):
-    return elements.MainTemplate()
+    return pages.MainPage()
+
+
+@route("/about")
+def root(request):
+    return pages.AboutPage()
+
+
+@route("/contact")
+def root(request):
+    return pages.ContactPage()
+
+
+@route("/search-results")
+def root(request):
+    return pages.SearchResultsPage()
 
 
 @route("/assets/")
