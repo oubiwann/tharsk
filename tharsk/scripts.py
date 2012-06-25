e0f4d557633aaadc8c2a5c31c122ba3093c2b84b
Duncan McGreggor <duncan@dreamhost.com>
2012-06-25 01:18:40 -0400

    Implemented importer and exporter scripts.

--- tharsk/scripts.py
+++ tharsk/scripts.py
@@ -1,9 +1,12 @@
-from twisted.internet import defer, reactor
+import sys
+
+from twisted.internet import reactor, threads
 from twisted.python import log
 
 import txmongo
+import txmongo.filter
 
-from tharsk import utils
+from tharsk import const, db, utils
 from tharsk.parsers import pdf
 
 
@@ -28,8 +31,8 @@ class ParseProtoCelticWordlistScript(Script):
 
 
 class AddProtoCelticKeywordsScript(Script):
-    inFile = open("./sources/pcl-eng.csv")
-    outFile = open("./sources/pcl-eng-keywords.csv", "w")
+    inFile = "./sources/pcl-eng.csv"
+    outFile = "./sources/pcl-eng-keywords.csv", "w"
     def run(self):
         super(AddProtoCelticKeywordsScript, self).run()
         reader = utils.UnicodeReader(self.inFile)
@@ -46,10 +49,13 @@ class TwistedScript(Script):
     """
     """
     def run(self):
+        log.startLogging(sys.stdout)
+        log.msg("Running the script ...")
         super(TwistedScript, self).run()
         reactor.run()
 
     def stop(self, ignore):
+        log.msg("Script finished.")
         reactor.stop()
 
     def logResult(self, result):
@@ -59,35 +65,85 @@ class TwistedScript(Script):
         log.msg(failure)
 
 
-class ImportDictionary(TwistedScript):
+class ImportProtCelticDictionary(TwistedScript):
     """
     """
-    def load(self, filename):
-        """
-        Note that 'filename' needs to be a .csv file with headers representing
-        the languages (three-letter code) that are getting imported. For a list
-        of supported languages and their codes, see tharsk.const.langMapper.
-        """
-        def insertData(self, conn, lang1, lang2, data):
+    csvFile = "./sources/pcl-eng-keywords.csv"
+
+    def doImport(self):
+
+        def indexEntries(ids, collection):
+            keyList = (txmongo.filter.ASCENDING("keywords") +
+                       txmongo.filter.ASCENDING("pcl"))
+            sortFields = txmongo.filter.sort(keyList)
+            d = collection.create_index(sortFields)
+            d.addErrback(self.logError)
+            d.addCallback(self.logResult)
+            return d
+
+        def insertData(csvReader, database):
+            collection = getattr(
+                database, const.databasePCLtoENGCollection)
+            data = []
+            log.msg("Preparing to iterate the CSV file ...")
+            for row in csvReader:
+                rowData = row
+                rowData["keywords"] = row["keywords"].split(",")
+                data.append(rowData)
+            log.msg("Finished iterating the CSV file.")
+            d = collection.insert(data)
+            d.addErrback(self.logError)
+            d.addCallback(indexEntries, collection)
+            return d
+
+        def loadData(database):
+            d = threads.deferToThread(
+                lambda: utils.UnicodeReader(self.csvFile))
+            d.addCallback(insertData, database)
+            return d
+
+        # get the database and load the data from a csv reader
+        d = db.getDatabase()
+        d.addCallback(loadData)
+        d.addErrback(self.logError)
+        return d
+
+    def run(self):
+        d = self.doImport()
+        d.addCallback(self.stop)
+        super(ImportProtCelticDictionary, self).run()
+
+
+class ExportProtCelticDictionary(TwistedScript):
+    """
+    """
+    def doExport(self):
+        
+        def logResults(docs):
+            for doc in docs:
+                log.msg(
+                    u"English: %s; Proto-Celtic: " % doc["eng"],
+                    doc["pcl"].encode("utf-8"))
+
+        def query(database):
             """
             A Twisted callback function.
             """
-            db = conn.tharsk
-            lang1Collection = getattr(db, lang1)
-            #d1 = lang1Collection.insert(...)
-            #d1.addCallback(self.logResult)
-            lang2Collection = getattr(db, lang2)
-            #d2 = lang2Collection.insert(...)
-            #d2.addCallback(self.logResult)
-            #return defer.DeferedList([d1, d2])
+            collection = getattr(
+                database, const.databasePCLtoENGCollection)
+            fields = {"eng": 1, "pcl": 1, "_id": 0}
+            filter = txmongo.filter.sort(txmongo.filter.ASCENDING("eng"))
+            d = collection.find(fields=fields, filter=filter)
+            d.addErrback(self.logError)
+            d.addCallback(logResults)
+            return d
 
-        reader = utils.UnicodeReader(filename)
-        # get the language codes from the header row
-
-        # add the data to the appropriate mongodb collections
-        d = txmongo.MongoConnection()
+        d = db.getDatabase()
+        d.addCallback(query)
         d.addErrback(self.logError)
-        d.addCallback(insertData, lang1, lang2, data)
-        d.addErrback(self.logError)
-        d.addCallback(self.stop)
         return d
+
+    def run(self):
+        d = self.doExport()
+        d.addCallback(self.stop)
+        super(ExportProtCelticDictionary, self).run()
