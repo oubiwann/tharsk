511f0822b8e348c073449cb1c2df554c49144be4
Duncan McGreggor <duncan@dreamhost.com>
2012-06-27 22:03:43 -0400

    Updated unicode and stemming utils.

--- tharsk/utils/__init__.py
+++ tharsk/utils/__init__.py
@@ -1,9 +1,15 @@
+# -*- coding: utf-8 -*-
 import itertools
 import re
+import unicodedata
 
 from stemming.porter2 import stem
 
 from tharsk import const
+
+
+punctuation = '!@#$%^&*()=+?.,<>";:/'
+punctuationPattern = re.compile('[%s]' % punctuation)
 
 
 def getDictionaryName(dictionary):
@@ -17,18 +23,60 @@ def getDictionaryNames():
         yield getDictionaryName(dictionary)
 
 
+def toUnicode(text):
+    if not isinstance(text, unicode):
+        try:
+            text = unicode(text)
+        except UnicodeDecodeError:
+            text = unicode(text.decode("utf-8"))
+    return text
+
+
+def normalizeUnicode(text):
+    text = re.sub(u"[Á]", u"A", toUnicode(text))
+    text = re.sub(u"[Ó]", u"O", text)
+    text = re.sub(u"[ç]", u"c", text)
+    text = re.sub(u"[û]", u"u", text)
+    text = re.sub(u"[ðþ]", u"th", text)
+    text = re.sub(u"[àäáâā]", u"a", text)
+    text = re.sub(u"[ûüùú]", u"u", text)
+    text = re.sub(u"[ôóõòö]", u"o", text)
+    text = re.sub(u"[èéê]", u"e", text)
+    text = re.sub(u"[ìÏíîĩīǐȋî]", u"i", text)
+    text = re.sub(u"[ñ]", u"n", text)
+    text = re.sub(u"[æ]", u"ae", text)
+    return unicodedata.normalize("NFKD", text)
+
+
 def getStems(wordList, skipWords=[], caseInsensitive=True):
+    """
+    Returns unicode.
+    """
     stems = []
-    punctuation = '!@#$%^&*()=+?.,<>";:'
-    pattern = re.compile('[%s]' % punctuation)
     for word in wordList:
         if word in skipWords:
             continue
-        word = pattern.sub("", word)
+        word = punctuationPattern.sub("", word)
+        word = toUnicode(word)
         if caseInsensitive:
             word = word.lower()
         stems.append(stem(word))
-    return set(stems)
+    return [x for x in sorted(list(set(stems))) if x]
+
+
+def getPCLStems(wordList, skipWords=[], caseInsensitive=True):
+    """
+    A dumb stemmer for Proto-Celtic words.
+
+    Returns unicode.
+    """
+    newWordList = []
+    for word in wordList:
+        word = punctuationPattern.sub("", word)
+        parts = [word] + word.split("-")
+        normalized = [normalizeUnicode(x).encode("utf-8") for x in parts]
+        newWordList.extend(parts + normalized)
+    return getStems(list(set(newWordList)))
 
 
 def getPermutations(iterable):
