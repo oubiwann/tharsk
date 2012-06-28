f40228c6ae3c846ae60fb69e6b54c2f7dc2955a6
Duncan McGreggor <duncan@dreamhost.com>
2012-06-27 22:03:14 -0400

    Added unicode and stemming unit tests.

--- tharsk/tests/test_utils.py
+++ tharsk/tests/test_utils.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import unittest
 
 from tharsk import utils
@@ -24,3 +25,32 @@ class UtilsTestCase(unittest.TestCase):
         expected = [('a',), ('b',), ('c',), ('a', 'b'), ('a', 'c'), ('b', 'c'),
                     ('a', 'b', 'c')]
         self.assertEqual(results, expected)
+
+    def test_getPCLStemsNoPunctuation(self):
+        wordList = "*(o)bb-nod".split()
+        results = utils.getPCLStems(wordList)
+        self.assertEqual(results, ['nod', 'obb', 'obb-nod'])
+
+    def test_getPCLStemsWithPunctuation(self):
+        wordList = "*(o)bb-nod-e/o".split()
+        results = utils.getPCLStems(wordList)
+        self.assertEqual(results, ['eo', 'nod', 'obb', 'obb-nod-eo'])
+
+    def test_getPCLStemsWithUnicode(self):
+        wordList = "*(ande-)stād-(ī-tu-)".split()
+        results = [x.encode("utf-8") for x in utils.getPCLStems(wordList)]
+        self.assertEqual(
+            results,
+            ['and', 'ande-stad-i-tu-', 'ande-stād-ī-tu-', 'i', 'stad', 'stād',
+             'tu', 'ī']
+            )
+
+    def test_normalizeUnicodePlain(self):
+        text = "text"
+        results = utils.normalizeUnicode(text)
+        self.assertEquals(results, "text")
+
+    def test_normalizeUnicodeNonPlain(self):
+        text = "ÓçóûàäèìðôüÁáéñõùâæêîòöúþ"
+        results = utils.normalizeUnicode(text)
+        self.assertEquals(results, "OcouaaeithouAaenouaaeeioouth")
