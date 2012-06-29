eb915d26a96091eb1e4a0a853c9f62341348fc70
Duncan McGreggor <duncan@dreamhost.com>
2012-06-29 00:39:49 -0400

    Improved the data import for PCL dictionary:
     * added an initial drop on the collection and its indices
     * fixed the index creation
     * added a method for checking the creation of indices
     * added logging on the various steps of the import

--- tharsk/scripts.py
+++ tharsk/scripts.py
@@ -1,6 +1,6 @@
 import sys
 
-from twisted.internet import reactor, threads
+from twisted.internet import defer, reactor, threads
 from twisted.python import log
 
 from tharsk import const, utils
@@ -96,38 +96,67 @@ class ImportProtCelticDictionary(TwistedScript):
         # instantiate the collection model
         model = collection.ProtoCelticDictionaryV1()
 
-        def indexEntries(ids):
-            keyList = (model.filter.ASCENDING("keywords") +
-                       model.filter.ASCENDING("pcl"))
-            sortFields = model.filter.sort(keyList)
-            d = model.collection.create_index(sortFields)
+        def checkIndices(listResult):
+            for result in listResult:
+                if result[0]:
+                    log.msg("Successfully created index '%s'." % result[1])
+                else:
+                    log.err("Could not create index '%s'." % result[1])
+
+        def createIndices(ids):
+            log.msg("Inserted %s documents." % len(ids))
+            indexedFields = ["pcl_keywords", "eng_keywords"]
+            deferreds = []
+            for fieldName in indexedFields:
+                keyList = model.filter.ASCENDING(fieldName)
+                sortFields = model.filter.sort(keyList)
+                deferreds.append(model.collection.create_index(sortFields))
+            d = defer.DeferredList(deferreds)
             d.addErrback(self.logError)
-            d.addCallback(self.logResult)
+            d.addCallback(checkIndices)
             return d
 
         def insertData(csvReader):
             data = []
+            counter = 0
             log.msg("Preparing to iterate the CSV file ...")
             for row in csvReader:
+                counter += 1
                 rowData = row
                 rowData["pcl-keywords"] = row["pcl-keywords"].split(",")
                 rowData["eng-keywords"] = row["eng-keywords"].split(",")
                 data.append(rowData)
-            log.msg("Finished iterating the CSV file.")
+            log.msg("Finished iterating the CSV file (read %s rows)." % (
+                counter,))
             d = model.collection.insert(data)
             d.addErrback(self.logError)
-            d.addCallback(indexEntries)
+            d.addCallback(createIndices)
             return d
 
-        def loadData(database):
+        def loadData(response):
+            if response["ok"] == 1:
+                log.msg(response["msg"])
             d = threads.deferToThread(
                 lambda: unicsv.UnicodeReader(self.csvFile))
             d.addCallback(insertData)
             return d
 
+        def dropIndexes(noValue):
+            d = model.collection
+            d = model.collection.drop_indexes()
+            d.addErrback(log.err)
+            d.addCallback(loadData)
+            return d
+
+        def dropCollection(database):
+            d = model.collection.drop(safe=True)
+            d.addErrback(log.msg)
+            d.addCallback(dropIndexes)
+            return d
+
         # get the database and load the data from a csv reader
         d = model.getDB()
-        d.addCallback(loadData)
+        d.addCallback(dropCollection)
         d.addErrback(self.logError)
         return d
 
@@ -145,6 +174,8 @@ class ExportProtCelticDictionary(TwistedScript):
         model = collection.ProtoCelticDictionaryV1()
 
         def logResults(docs):
+            if len(docs) == 0:
+                log.msg("Query returned no documents.")
             for doc in docs:
                 log.msg(
                     u"English: %s; Proto-Celtic: " % doc["eng"],
