4c48235e5e4d81e5e8cf9c367508279fd3f5bf68
Duncan McGreggor <duncan@dreamhost.com>
2012-07-01 18:30:49 -0400

    Updated unit test method names to match coding standard.

--- tharsk/utils/parsers/tests/test_pdf.py
+++ tharsk/utils/parsers/tests/test_pdf.py
@@ -10,17 +10,17 @@ class TabbedConverterTestCase(unittest.TestCase):
     def setUp(self):
         self.converter = pdf.TabbedConverter(None, None)
 
-    def test_process_first_item(self):
+    def test_processFirstItem(self):
         result = self.converter.processFirstItem("tharsk")
         expected = "tharsk                                  "
         self.assertEqual(result, expected)
 
-    def test_process_second_item(self):
+    def test_processSecondItem(self):
         result = self.converter.processSecondItem("to ask")
         expected = "to ask\n"
         self.assertEqual(result, expected)
 
-    def test_format_row(self):
+    def test_formatRow(self):
         result = self.converter.formatRow("tharsk", "to ask")
         expected = "tharsk                                  to ask\n"
         self.assertEqual(result, expected)
@@ -32,17 +32,17 @@ class CSVConverterTestCase(unittest.TestCase):
     def setUp(self):
         self.converter = pdf.CSVConverter(None, None)
 
-    def test_process_first_item(self):
+    def test_processFirstItem(self):
         result = self.converter.processFirstItem("tharsk")
         expected = '"tharsk"; '
         self.assertEqual(result, expected)
 
-    def test_process_second_item(self):
+    def test_processSecondItem(self):
         result = self.converter.processSecondItem("to ask")
         expected = '"to ask"\n'
         self.assertEqual(result, expected)
 
-    def test_format_row(self):
+    def test_formatRow(self):
         result = self.converter.formatRow("tharsk", "to ask")
         expected = '"tharsk"; "to ask"\n'
         self.assertEqual(result, expected)
